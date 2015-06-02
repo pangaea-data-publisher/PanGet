90a2c333895f666d390d6f864667315bf918cb0e
Rainer Sieger <rsieger@pangaea.de>
2015-06-02 23:14:22 +0200

    macdeploy.sh - Qt 5.4.1 -> Qt 5.4.2

--- macdeploy.sh
+++ macdeploy.sh
@@ -7,7 +7,7 @@ echo - macdeployqt
 cd ~/Development/GitHub/PanGet
 
 rm -R '../../Distribution/PanGet/PanGet.app'
-cp -R './build-PanGet-Desktop_Qt_5_4_1_clang_64bit-Release/PanGet.app' '../../Distribution/PanGet/PanGet.app'
+cp -R './build-PanGet-Desktop_Qt_5_4_2_clang_64bit-Release/PanGet.app' '../../Distribution/PanGet/PanGet.app'
 cp './trunk/Resources/Info.plist' '../../Distribution/PanGet/PanGet.app/Contents/Info.plist'
 
 /Developer/Qt/5.4/clang_64/bin/macdeployqt '../../Distribution/PanGet/PanGet.app'
